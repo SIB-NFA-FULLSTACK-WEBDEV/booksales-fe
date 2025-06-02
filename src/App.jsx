702d220c5286863aacd28ae787a18cc787b98444
Ramdhan Ariansyah <ramdhanrian12345@gmail.com>
2025-06-02 21:55:32 +0700

    feat: add create and read for genre and author

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import AdminLayout from "./layouts/admin";
 import Dashboard from "./pages/admin";
 import AdminBooks from "./pages/admin/books";
 import BookCreate from "./pages/admin/books/create";
+import AdminAuthors from "./pages/admin/authors";
+import AuthorCreate from "./pages/admin/authors/create";
+import AdminGenres from "./pages/admin/genres";
+import GenreCreate from "./pages/admin/genres/create";
 
 function App() {
   return (
@@ -28,10 +32,25 @@ function App() {
           <Route path="admin" element={<AdminLayout />}>
             <Route index element={<Dashboard />} />
 
+            {/* Books */}
             <Route path="books">
               <Route index element={<AdminBooks />} />
               <Route path="create" element={<BookCreate />} />
               {/* <Route path=":id/edit" element={<EditBook />} /> */}
+            </Route>
+
+            {/* Authors */}
+            <Route path="authors">
+              <Route index element={<AdminAuthors />} />
+              <Route path="create" element={<AuthorCreate />} />
+              {/* <Route path=":id/edit" element={<AuthorEdit />} /> */}
+            </Route>
+
+            {/* Genres */}
+            <Route path="genres">
+              <Route index element={<AdminGenres />} />
+              <Route path="create" element={<GenreCreate />} />
+              {/* <Route path=":id/edit" element={<AuthorEdit />} /> */}
             </Route>
           </Route>
         </Routes>
--- src/_services/authors.js
+++ src/_services/authors.js
@@ -4,3 +4,13 @@ export const getAuthors = async () => {
   const { data } = await API.get("/authors");
   return data.data;
 };
+
+export const createAuthor = async (data) => {
+  try {
+    const response = await API.post("/authors", data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
