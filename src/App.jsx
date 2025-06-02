5d4087f4b2e7643cf5e3c4f98b11660b3b863d6c
Ramdhan Ariansyah <ramdhanrian12345@gmail.com>
2025-06-02 20:23:10 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PublicLayout from "./layouts/public";
+import Home from "./pages/public";
+import Books from "./pages/public/books";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
+import AdminLayout from "./layouts/admin";
+import Dashboard from "./pages/admin";
+import AdminBooks from "./pages/admin/books";
+import BookCreate from "./pages/admin/books/create";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* Public */}
+          <Route element={<PublicLayout />}>
+            <Route index element={<Home />} />
+            <Route path="books" element={<Books />} />
+          </Route>
+
+          {/* Auth */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+
+          {/* Admin */}
+          <Route path="admin" element={<AdminLayout />}>
+            <Route index element={<Dashboard />} />
+
+            <Route path="books">
+              <Route index element={<AdminBooks />} />
+              <Route path="create" element={<BookCreate />} />
+              {/* <Route path=":id/edit" element={<EditBook />} /> */}
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
